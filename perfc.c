9e7d1fda68332da14fbe36ac74df0abac0474119
Philipp Trommler <philipp.trommler@mailbox.tu-dresden.de>
2016-03-11 13:18:33 +0100

    Added/Corrected licence files and file headers and pulled submodule to most recent version.

--- perfc.c
+++ perfc.c
@@ -1,22 +1,28 @@
 /*
-libKhwPlugin.so,
-a library to count kernel hardware events in VampirTrace
-Copyright (C) 2010 TU Dresden, ZIH
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
+ * Copyright (c) 2016, Technische Universität Dresden, Germany
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+ *    or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifdef VT
@@ -354,8 +360,8 @@ uint64_t get_value(int index){
 #ifdef VT
 
 vt_plugin_cntr_metric_info * get_event_info(char * event_name){
-	vt_plugin_cntr_metric_info * return_values;
-	int ret;
+    vt_plugin_cntr_metric_info * return_values;
+    int ret;
   struct perf_event_attr attr;
   char unit[256]="";
   double scale;
@@ -367,20 +373,20 @@ vt_plugin_cntr_metric_info * get_event_info(char * event_name){
     return NULL;
   }
   return_values=
-			malloc(2 * sizeof(vt_plugin_cntr_metric_info) );
+            malloc(2 * sizeof(vt_plugin_cntr_metric_info) );
   if (return_values==NULL){
     fprintf(stderr, "VampirTrace PerfC Plugin: failed to allocate memory for passing information to VT.\n");
     return NULL;
   }
-	return_values[0].name=strdup(event_name);
-	if (unit[0]=='\0')
-	  return_values[0].unit=NULL;
-	else
+    return_values[0].name=strdup(event_name);
+    if (unit[0]=='\0')
+      return_values[0].unit=NULL;
+    else
     return_values[0].unit=strdup(unit);
-	return_values[0].cntr_property=VT_PLUGIN_CNTR_LAST|VT_PLUGIN_CNTR_START|
+    return_values[0].cntr_property=VT_PLUGIN_CNTR_LAST|VT_PLUGIN_CNTR_START|
      VT_PLUGIN_CNTR_DOUBLE;
-	return_values[1].name=NULL;
-	return return_values;
+    return_values[1].name=NULL;
+    return return_values;
 }
 
 
